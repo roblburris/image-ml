843b174cac8f7e2f83865d940eddba88d0a82d4e
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-13 21:18:45 -0700

    Add appropriate fonts and appbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import AppBar from './components/AppBar';
 import CenterWrapper from './components/CenterWrapper';
 
 function App() {
   return (
+    <>
+    <AppBar/>
     <div className="App">
       <CenterWrapper/>
     </div>
+    </>
   );
 }
 
