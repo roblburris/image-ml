43d3a059abfb7cbcddeb3bd16ebd0cda726c89ba
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-13 21:42:17 -0700

    Add some types and states

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from './components/AppBar';
 import CenterWrapper from './components/CenterWrapper';
+import { homePageState } from './types';
 
 function App() {
+  var [state, setState] = useState(homePageState);
+
   return (
     <>
     <AppBar/>
     <div className="App">
-      <CenterWrapper/>
+      <CenterWrapper setState={setState} state={state}/>
     </div>
     </>
   );
--- frontend/src/components/CenterWrapper.tsx
+++ frontend/src/components/CenterWrapper.tsx
@@ -4,7 +4,12 @@ import './CenterWrapper.css';
 import NormalDescription from './Description/NormalDescription';
 import UploadButton from './Button/UploadButton';
 
-function CenterWrapper() {
+type CenterWrapperProps = {
+  state: number,
+  setState: React.Dispatch<React.SetStateAction<number>>,
+}
+
+function CenterWrapper({state, setState}: CenterWrapperProps) {
   return (
     // Should conditionally render UploadButton (import Progress and stuff from button folder)
     // Should conditionall render Description (normally do normal, but use the image one when processed)
