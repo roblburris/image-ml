d814c6e3efe8ea4cdbaa2e3bfb823a542465add5
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-05-15 22:32:05 -0700

    added progress bar system

--- frontend/src/components/Description/ProgressBar.tsx
+++ frontend/src/components/Description/ProgressBar.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Description.css';
 
+const getProgressStyles = (progress: number) => { //Take a number and calculate the styles in the progress bar
+
+    return {
+        backgroundColor: "#3286E9",
+        borderRadius: "100vw",
+        width: `${progress}%`,
+        height: "30px",
+        minWidth: `${50}px`,
+        minHeight: "30px"
+    
+    } 
+}
+
 function ProgressBar() {
     return (
         <div className="progress">
-            Progress... Bar Coming Soon!
+            <div style={getProgressStyles(50)}/> 
         </div>
     )
 }
