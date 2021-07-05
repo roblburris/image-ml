d11c725d2b26b81aca64f3b69a240ea6e89bc14e
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2021-07-05 15:38:04 -0700

    Update runpy.js

--- backend/src/runpy.js
+++ backend/src/runpy.js
@@ -6,14 +6,20 @@ export default function(filename) {
 	return new Promise((resolve, reject) => {
 		let dataToSend = "";
 	
-		const py = spawn('python', [join(pyPath, "ml.py"), filename]);
+		const py = spawn('python', [join(pyPath, "test.py"), filename]);
+		console.log(py);
 
 		py.stdout.on('data', (data) => {
 			dataToSend = data.toString();
 		});
+
+		py.stderr.on("data", (data) => {
+			console.log(data.toString())
+		});
 		
 		py.on('close', (code) => {
 			console.log("Exit code: " + code);
+			if(code != 0) return resolve(code);
 			resolve(dataToSend);
 		});
 
