4a522b32317ea69bf8b819749e4b0644386393cc
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2021-07-05 15:37:40 -0700

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,5 +1,7 @@
 import { join } from "path";
 
 export const PORT = process.env.PORT || 8080;
-export const pyPath = join(new URL("./", import.meta.url).pathname, "python");
-export const tmpPath = join(new URL("./", import.meta.url).pathname, "..", "tmp");
+const py = join(new URL("./", import.meta.url).pathname, "python");
+export const pyPath = process.platform === "win32" ? py.substr(1, py.length) : py;
+const temp = join(new URL("./", import.meta.url).pathname, "..", "tmp");
+export const tmpPath = process.platform === "win32" ? temp.substr(1, temp.length) : temp;
